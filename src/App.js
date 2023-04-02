3f13fae490c4cdd9f621fa1f6b67f9da15f2bf1c
isa-bandit <isaabdraev5@gmail.com>
2023-04-02 18:18:34 +0600

    add files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Main from './component/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Main.jsx
@@ -0,0 +1,11 @@
+import React  from "react"  
+import './main.css'
+
+export default function Main() {
+    return(
+        <div className="divchik">
+         <h1 className="isa">Isa-mashina</h1>
+         
+        </div>
+    )
+}
